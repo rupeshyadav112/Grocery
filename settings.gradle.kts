8ef19b4536dffa93de5ad227720769f748cd728e
Rupesh Kumar <135962952+rupeshyadav112@users.noreply.github.com>
2024-09-30 08:20:08 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // Add any other necessary repositories
+
+    }
+}
+
+rootProject.name = "Grocery"
+include(":app")
